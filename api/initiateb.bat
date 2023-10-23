6f0296046a8436b4738f808dfcfff9e8c6344b61
Laksmi Prasad Nampally <nampallylakshmiprasad@gmail.com>
2023-10-23 16:30:00 +0530

    check

--- api/initiateb.bat
+++ api/initiateb.bat
@@ -1,14 +1,14 @@
 @echo off
 
 if not exist "project" (
-            echo virtual ENV not found...
-            echo virtual env is being created
+            echo Creating Environment
             python -m venv project
         )
-echo virtual Found, activating virtual ENV.
+echo Activating Environment.
 call project\Scripts\activate.bat
 echo installing packages
 pip install -r requirements.txt
+cls
 echo starting server
 uvicorn app:app --port 8000 --reload
 pause
